c7248f2f9d1e6a5c920865ff8a5765bdea4e7fde
leo <contractsmart9@gmail.com>
2025-04-03 13:43:37 +0200

    initial structure

--- src/pages/homepage/HomepageTexture.js
+++ src/pages/homepage/HomepageTexture.js
@@ -7,15 +7,15 @@ const HomePageTexture = () => {
   return (
     <div className="absolute w-full h-screen overflow-hidden">      
       <header className="top-0 left-0 w-4/5 mx-auto p-5 flex justify-between items-center z-10">
-        <img src="/assets/images/header-logo.png" alt="Integrity Logo" className="h-9" />
+        <img src="/assets/images/header-logo.png" alt="Integrity Logo" className="h-9 cursor-pointer" />
         <button className="bg-white text-black rounded-full px-6 py-2 text-sm font-medium hover:bg-opacity-90">
-          Ask an Invitation
+          Join the Waitlist
         </button>
       </header>
 
       <div className=" flex flex-col items-center justify-center h-screen text-white text-center">
         <img src="/assets/images/center-logo.png" alt="Integrity Symbol" className="w-20 h-20 mb-5 drop-shadow-lg" />
-        <h1 className="text-4xl font-semibold">Discover</h1>
+        <h1 className="text-4xl font-semibold">Preview Integrity</h1>
         <p className="text-lg text-gray-400 max-w-md mt-3">
           The World's First On-Chain Bitcoin Backed Reward Protocol for Web3 Enterprises
         </p>
@@ -38,7 +38,7 @@ const HomePageTexture = () => {
         </div>
         
         <button className="mt-4 px-6 py-3 border border-white rounded-full hover:bg-white hover:text-black transition">
-          Enter your invitation code →
+          Enter Website →
         </button>
       </div>
     </div>
