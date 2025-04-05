2b9b4400739aa56d275cd429b4a7c04ce5dd8547
Locklee-ss <locklee0418@proton.me>
2025-04-05 16:52:38 -0700

    for backup

--- src/pages/LP-NFT/index.js
+++ src/pages/LP-NFT/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Page1 from "./page1";
-import Page3 from "./page3";
+// import Page3 from "./page3";
 import Page5 from "./page5";
 import Page7 from "./page7";
 import Page8 from "./page8";
@@ -10,7 +10,7 @@ const LPNFTPage = () => {
   return (
     <>
       <Page1 />
-      <Page3 />
+      {/* <Page3 /> */}
       <Page5 />
       <Page7 />
       <Page8 />
