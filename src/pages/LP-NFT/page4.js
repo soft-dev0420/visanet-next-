1d529e6cda1b8611930ba37c9e15465c1508c090
Locklee-ss <locklee0418@proton.me>
2025-04-06 15:38:26 -0700

    page4 modify

--- src/pages/LP-NFT/page4.js
+++ src/pages/LP-NFT/page4.js
@@ -2,30 +2,33 @@ import React from "react";
 
 const Page4 = () => {
   return (
-    <div className=" bg-black text-white relative">
+    <div className="bg-[#111113] text-white relative py-[50px] md:py-[200px] px-5">
       {/* How it works button */}
-      <div className="flex items-center justify-center gap-2 mb-1">
+      <div className="flex items-start md:justify-center gap-2 mb-1">
         <button className="flex items-center space-x-2 text-gray-300 hover:text-white transition-colors">
           <img alt="green-warning" src="./assets/images/icons/pickaxe.svg"/>
-          <span className="text-white-400 text-[17px]">How it works?</span>
+          <span className="text-white-400 text-[14px] font-bold">How it works?</span>
         </button>
       </div>
 
       {/* Hero Section */}
-      <div className="container mx-auto px-4 pt-4 pb-16">
-        <div className="max-w-4xl mx-auto text-center">
-          <h1 className="text-4xl md:text-6xl text-center font-light max-w-4xl  leading-tight mb-6 ">
-            Add Programmable, On-<br/>Chain Bitcoin Mining Logic to <br/>All Your NFTs Collections
+      <div className="container mx-auto md:px-4 pt-4">
+        <div className="max-w-4xl mx-auto text-left md:text-center">
+          <h1 className="text-3xl md:text-6xl font-light max-w-4xl  leading-tight mb-5 ">
+            Add Programmable, On-Chain Bitcoin Mining Logic to All Your NFTs Collections
           </h1>
-          <p className="text-gray-400 text-lg mb-4 max-w-[650px] text-[17px] mx-auto">
-            from Art and Collectibles to PFPs, Music, Tickets, and Membership NFTs.<br/>
-            Drive engagement, stabilize digital asset ecosystems, and support long-term <br/>community value through verifiable, Bitcoin-based utility.
+          <p className="text-gray-400 mb-4 max-w-[585px] text-[16px] md:text-lg mx-auto">
+            from Art and Collectibles to PFPs, Music, Tickets, and Membership NFTs.
+            Drive engagement, stabilize digital asset ecosystems, and support long-term community value through verifiable, Bitcoin-based utility.
           </p>
         </div>
 
         {/* Feature Cards */}
-        <div className="flex flex-wrap justify-center gap-4">
+        <div className="hidden md:flex flex-wrap justify-center gap-4">
           <img alt="ss" src="./assets/images/cycle.png"/>
+        </div>
+        <div className="md:hidden flex-wrap justify-center gap-4">
+          <img alt="ss" src="./assets/images/cycle_horizontal.png"/>
         </div>
 
       </div>
